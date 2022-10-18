bf3f82e1cc1e84c482fe8d35e94f56cd21ccde91
Adewole Muhammed <adewoleadetunji77@gmail.com>
2022-10-18 14:06:42 +0100

    Faq page is done

--- App.js
+++ App.js
@@ -4,3 +4,12 @@ let body = document.querySelector('body');
 toggle.addEventListener('click', function() {
     body.classList.toggle('open')
 });
+
+// function for the accordion in the FAQ page
+let acc = document.getElementsByClassName("accordion");
+
+    for(i = 0; i < acc.length; i++) {
+        acc[i].addEventListener("click", function() {
+            this.classList.toggle("active");
+        })
+    }
