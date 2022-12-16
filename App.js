681075da870f6bf640446fbd7a352808ca7657cc
Adewole Muhammed <adewoleadetunji77@gmail.com>
2022-12-16 01:53:23 +0100

    contact form fixed

--- App.js
+++ App.js
@@ -14,3 +14,22 @@ let acc = document.getElementsByClassName("accordion");
             this.classList.toggle("active");
         })
     };
+
+
+// function for the contact form
+function sendEmail(){
+    Email.send({
+        Host : "smtp.elasticemail.com",
+        Username : "adewoleadetunji77@gmail.com",
+        Password : "6095014D0404DA797BD7205088114DE1234D",
+        To : 'adewoleadetunji77@gmail.com',
+        From : document.getElementById("email").value,
+        Subject : "New contact form filled from Ensome",
+        Body : "Name: " + document.getElementById("name").value
+                + " <br> Email: " + document.getElementById("email").value
+                + " <br> Theme: " + document.getElementById("theme").value
+                + " <br> Message: " + document.getElementById("message").value
+    }).then(
+      message => alert("Message sent successfully, Thank you for choosing Ensome.")
+    );
+}
